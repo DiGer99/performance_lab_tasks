72328383b1c86f4b3d3998cf4dd4edd918fa7d1f
DiGer99 <gerasimov.dima891@gmail.com>
2025-07-06 16:43:57 +0300

    edit task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,47 +1,17 @@
 import typer
+from task1.utils_task1 import way_array
 
 
 app = typer.Typer()
 
 
-class Node:
-    head = None
-
-    def __init__(self, value):
-        self.value = value
-        self.next_node = None
-
-        if Node.head is None:
-            Node.head = self
-
-
 @app.command()
-def way_array(
+def main(
     n: int = typer.Argument(..., help="Длина массива"),
     m: int = typer.Argument(..., help="Шаг обхода"),
 ):
-
-    Node.head = None
-    head = Node(1)
-    current = head
-    res = []
-    for num in range(2, n + 1):
-        current.next_node = Node(num)
-        current = current.next_node
-
-    current.next_node = head
-    res.append(head.value)
-    current = head
-
-    for i in range(m - 1):
-        current = current.next_node
-
-    while current != head:
-        res.append(current.value)
-        for i in range(m - 1):
-            current = current.next_node
-
-    typer.echo("".join(map(str, res)))
+    res = way_array(n, m)
+    typer.echo(res)
 
 
 if __name__ == "__main__":
